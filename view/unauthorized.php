783d62149ce838f15d8e8f386e47301746c46011
Maddo <mjablecki@gmail.com>
2012-06-15 22:18:07 -0300

    Spelling error

--- view/unauthorized.php
+++ view/unauthorized.php
@@ -1,4 +1,4 @@
 <div class="hero-unit">
 	<h1>UNAUTHORIZED UNAUTHORIZED UNAUTHORIZED</h1>
-	<p>Just kidding, it's no that bad. But really, you shouldn't try to come here without <a href="/user/login">loggin in</a> first.</p>
+	<p>Just kidding, it's no that bad. But really, you shouldn't try to come here without <a href="/user/login">logging in</a> first.</p>
 </div>
